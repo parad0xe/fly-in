d2c7aafa96f93902f9243c5b79e290aed90fd4c8
parad0xe <parad0xe@protonmail.com>
2026-02-18 17:25:41 +0000

    format: flyin.model.graph

--- flyin/models/graph.py
+++ flyin/models/graph.py
@@ -5,6 +5,16 @@ from flyin.models.link import Link
 
 
 class Graph(BaseModel):
+    """
+    Represents the complete network topology for drone routing.
+
+    Attributes:
+        hubs: List of all hubs (nodes) within the graph.
+        links: List of all connections (edges) between hubs.
+        start_hub: The designated hub where the drone fleet starts.
+        end_hub: The designated target hub for the drone fleet.
+    """
+
     model_config = ConfigDict(extra="forbid", frozen=True)
 
     hubs: list[Hub]
