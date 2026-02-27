2cd6427494beea6df8f57d7bea62c454d6df2249
parad0xe <parad0xe@protonmail.com>
2026-02-27 15:38:18 +0100

    new(solver algorithm process): add file with comments of solving algorithm process only

---
+++ flyin/solver.py
@@ -0,0 +1,43 @@
+"""
+
+h: int = height
+f: int = flow
+u: int = capacity
+
+s = start
+t = target
+v = vextex [!s & !t]
+
+e = edge = (v, w)
+
+residual_capacity = u(e) - f(e)
+
+αf(v): int = f_in(v) - f_out(v) = excess
+
+Initialization:
+    h(s) = nb_drones
+    h(t) = 0
+    h(v) = 0
+
+    e(s) = 0
+    e(t) = 0
+    e(v) = 0
+
+Invariants:
+    h(s) = abs(nb_drones)
+    h(t) = 0
+    ∀e(v, w); h(v) <= h(w) + 1
+
+Subroutines:
+    push(v):
+
+
+Preflow:
+    initial push
+
+Main Loop:
+    while excess(v) > 0:
+        cv = max(h(v))
+
+
+"""
